a2c32db6071f27bd05006dcf23a5fbdeaa9208ef
Yves Le Pors <y.lepors@novediagroup.com>
2014-08-01 15:13:02 +0200

    Initial Commit

---
+++ QuotesDataExample/AppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.h
+//  QuotesDataExample
+//
+//  Created by Yves Le Pors on 01/08/2014.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
---
+++ QuotesDataExample/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  QuotesDataExample
+//
+//  Created by Yves Le Pors on 01/08/2014.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
